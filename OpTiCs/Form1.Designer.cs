9f5152994d496802b3fd830317547f2d091466c4
sh-anousheh <sh-anousheh@users.noreply.github.com>
2020-01-18 10:12:43 -0500

    Initial commit

---
+++ OpTiCs/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace OpTiCs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(199, 77);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(469, 303);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button1.Location = new System.Drawing.Point(470, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 25);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 355);
+            this.listBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "The Order Of Poins Are:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(738, 77);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(314, 303);
+            this.listBox2.TabIndex = 4;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(738, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(738, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(196, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "MinPts";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(196, 25);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(25, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Eps";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(277, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(277, 22);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(441, 49);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Second Eps";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(512, 46);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1064, 392);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ OpTiCs/Form1.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+using System.IO;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace OpTiCs
+{
+    public partial class Form1 : Form
+    {
+        static int MinPts;
+        static double Eps;
+        static double secondEps;
+        static int RowNum;
+        static int ColNum;
+        int[,] A;
+        class Object
+        {
+            public int Name;
+            public double B;
+            public double G;
+            public double R;
+            public bool Processed = false;
+            public double reachability_distance = -1; //UNDEFINED
+            public double core_distance = -1; //UNDEFINED
+            public int ClusterId;
+            public Object(double _B, double _G, double _R, int _Name)
+            {
+                B = _B;
+                G = _G;
+                R = _R;
+                Name = _Name;
+            }
+        }
+        class setOfObjects : List<Object>
+        {
+        }
+        #region Read Exel
+        static int[,] Read(string S)
+        {
+            string d = Directory.GetCurrentDirectory(); //Copy the exel file in Debug folder
+            Microsoft.Office.Interop.Excel.Application _excelApp = new Microsoft.Office.Interop.Excel.Application();
+            _excelApp.Visible = true;
+            string fileName = d + S;
+            Workbook workbook = _excelApp.Workbooks.Open(fileName,
+                Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                Type.Missing, Type.Missing);
+            Worksheet worksheet = (Worksheet)workbook.Worksheets[1];
+            Range excelRange = worksheet.UsedRange;
+            object[,] valueArray = (object[,])excelRange.get_Value(
+                        XlRangeValueDataType.xlRangeValueDefault);
+            RowNum = valueArray.GetLength(0);
+            ColNum = valueArray.GetLength(1);
+            string[] ar = new string[RowNum + 1];
+            for (int i = 1; i <= RowNum; i++)
+                ar[i] = (valueArray[i, 1].ToString());
+            string[][] ar1 = new string[RowNum + 1][];
+            for (int i = 1; i <= RowNum; i++)
+            {
+                string v = ar[i];
+                ar1[i] = v.Split(',');
+            }
+            int[,] arr = new int[RowNum + 1, 4];
+            for (int i = 1; i <= RowNum; i++)
+                for (int j = 0; j < 3; j++)
+                    arr[i, j + 1] = int.Parse(ar1[i][j]);
+            try { workbook.Close(false, Type.Missing, Type.Missing); }
+            catch { }
+            try { _excelApp.Quit(); }
+            catch { }
+            return arr;
+        }
+        #endregion
+        static double Dist(Object o1, Object o2)
+        {
+            double dR = o2.R - o1.R;
+            double dG = o2.G - o1.G;
+            double dB = o2.B - o1.B;
+            double dist = Math.Sqrt(dR * dR + dG * dG + dB * dB);
+            return dist;
+        }
+        static setOfObjects NeighborQuery(setOfObjects SetOfObjects, Object Object)
+        {
+            setOfObjects neighbors = new setOfObjects();
+            for (int i = 0; i < SetOfObjects.Count; i++)
+            {
+                double dist = Dist(Object, SetOfObjects[i]);
+                if (dist <= Eps)
+                    neighbors.Add(SetOfObjects[i]);
+            }
+            return neighbors;
+        }
+        static double setCoreDistance(setOfObjects neighbors, Object Object)
+        {
+            if (neighbors.Count < MinPts)
+                return -1;
+            else
+            {
+                List<double> Distances = new List<double>();
+                for (int i = 0; i < neighbors.Count; i++)
+                    Distances.Add(Dist(Object, neighbors[i]));
+                Distances.Sort();
+                return Distances[MinPts - 1];/// MinPts omin hamsaye nazdik (ba khode Object hesab shode)
+            }
+
+        }
+        static Queue<Object> updateOrderSeeds(Queue<Object> OrderSeeds, setOfObjects neighbors, Object CenterObject)
+        {
+            double c_dist = CenterObject.core_distance;
+            foreach (Object Object in neighbors)
+                if (!Object.Processed)
+                {
+                    double new_r_dist;
+                    if (c_dist > Dist(CenterObject, Object))
+                        new_r_dist = c_dist;
+                    else
+                        new_r_dist = Dist(CenterObject, Object);
+                    if (Object.reachability_distance == -1)
+                    {
+                        Object.reachability_distance = new_r_dist;
+                        Object.reachability_distance = new_r_dist;
+                        OrderSeeds.Enqueue(Object);
+                    }
+                    else if (new_r_dist < Object.reachability_distance)
+                    {
+                        Object.reachability_distance = new_r_dist;
+                        Queue<Object> Temp = new Queue<Object>(OrderSeeds.OrderBy(p => p.reachability_distance));
+                        OrderSeeds.Clear();
+                        OrderSeeds = Temp;
+                    }
+                }
+            return OrderSeeds;
+        }
+        static setOfObjects ExpandClusterOrder(setOfObjects SetOfObjects, Object Object, setOfObjects OrderedFile)
+        {
+            setOfObjects neighbors = NeighborQuery(SetOfObjects, Object);
+            Object.Processed = true;
+            Object.reachability_distance = -1;
+            Object.core_distance = setCoreDistance(neighbors, Object);  // core 0 shode ye mosht node ruye ham hastan
+            OrderedFile.Add(Object);
+            Queue<Object> OrderSeeds = new Queue<Object>();
+            if (Object.core_distance != -1)
+            {
+                OrderSeeds = updateOrderSeeds(OrderSeeds, neighbors, Object);
+                while (OrderSeeds.Count > 0)
+                {
+                    Object currentObject = OrderSeeds.Dequeue();
+                    setOfObjects currentneighbors = NeighborQuery(SetOfObjects, currentObject);
+                    currentObject.Processed = true;
+                    currentObject.core_distance = setCoreDistance(currentneighbors, currentObject);
+                    OrderedFile.Add(currentObject);
+                    if (currentObject.core_distance != -1)
+                        OrderSeeds = updateOrderSeeds(OrderSeeds, currentneighbors, currentObject);
+                }
+            }
+            return OrderedFile;
+        }
+        static void ExtractDBSCANClustering(setOfObjects OrderedFile)
+        {
+            int ClusterId = 0;
+            for (int i = 0; i < OrderedFile.Count; i++)
+            {
+                Object Object = OrderedFile[i];
+                if (Object.reachability_distance > secondEps || Object.reachability_distance == -1)
+                    if (Object.core_distance <= secondEps && Object.core_distance != -1)
+                    {
+                        ClusterId = ClusterId + 1;
+                        Object.ClusterId = ClusterId;
+                    }
+                    else
+                        Object.ClusterId = -1; //NOISE
+                else
+                    Object.ClusterId = ClusterId;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            A = Read("\\HW2_data.xlsx");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+            textBox3.Enabled = false;
+            button1.Enabled = false;
+            MinPts = Convert.ToInt16(textBox1.Text);
+            Eps = Convert.ToDouble(textBox2.Text);
+            secondEps = Convert.ToDouble(textBox3.Text);
+            setOfObjects SetOfObjects = new setOfObjects();
+            for (int i = 1; i <= RowNum; i++)
+                SetOfObjects.Add(new Object(A[i, 1]/255.0, A[i, 2]/255.0, A[i, 3]/255.0, i));
+            setOfObjects OrderedFile = new setOfObjects();
+            for (int i = 0; i < SetOfObjects.Count; i++)
+            {
+                Object Object = SetOfObjects[i];
+                if (!Object.Processed)
+                    OrderedFile = ExpandClusterOrder(SetOfObjects, Object, OrderedFile);
+            }
+            // OrderedFile.Clear();
+            int num = 1;
+            foreach (Object O in OrderedFile)
+            {
+                listBox1.Items.Add(string.Format("{0}.           {1}", num, O.Name));
+                num++;
+            }
+
+            ExtractDBSCANClustering(OrderedFile);
+
+            string d = Directory.GetCurrentDirectory();
+            chart1.ChartAreas[0].Axes[0].Title = "Points";
+            chart1.ChartAreas[0].Axes[1].Title = "Reachability Distance";
+            chart1.Series[0].Color = Color.Red;
+            chart1.Series[0].BorderWidth = 1;
+            chart1.ForeColor = Color.AliceBlue;
+            chart1.Series[0].ChartType = SeriesChartType.Column;
+
+            for (int i = 1; i < OrderedFile.Count; i++)
+            {
+                if (OrderedFile[i - 1].reachability_distance == -1)
+                    chart1.Series[0].Points.AddXY(i, Eps);
+                else
+                    chart1.Series[0].Points.AddXY(i, OrderedFile[i - 1].reachability_distance);
+                System.Windows.Forms.Application.DoEvents();
+            }
+            this.chart1.SaveImage(d + "\\chart.png", ChartImageFormat.Png);
+
+            List<setOfObjects> Clusters = new List<setOfObjects>();
+            int maxClusterId = OrderedFile.OrderBy(p => p.ClusterId).Last().ClusterId;
+            if (maxClusterId <= 0)
+                label2.Text = string.Format("All Points Are Noise!");
+            else
+                for (int i = 0; i < maxClusterId; i++)
+                    Clusters.Add(new setOfObjects());
+            foreach (Object O in SetOfObjects)
+            {
+                if (O.ClusterId > 0)
+                    Clusters[O.ClusterId - 1].Add(O);
+            }
+            label3.Text = string.Format("THe Number Of Clusters Are : {0}", Clusters.Count);
+            int sum = 0;
+            for (int i = 0; i < Clusters.Count; i++)
+            {
+                sum += Clusters[i].Count;
+                listBox2.Items.Add(string.Format("Cluster {0}    number of points : {1}", i + 1, Clusters[i].Count));
+            }
+            label2.Text = string.Format("{0} Points are Noise.", RowNum - sum);
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
